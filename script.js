09b8d655f34acdb2113ecb147299d746bf2235e4
Martin Dallos <tinypin14@gmail.com>
2022-12-14 18:43:28 +0100

    Finished game status checking functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // TODO Solve global player variables!!!
+    // * SOLUTION: create a players module IIFE with a creator factory function and 2 public player objects 
 let player1;
 let player2;
 const players = function(name, symbol, hasTheTurn) {
@@ -60,8 +61,6 @@ const flowControl = (function() {
         if (player1 || player2) return;
         player1 = players(player1Name, 'X', true);
         player2 = players(player2Name, 'O', false);
-        
-        console.log({player1, player2});
     }
 
     // * Taking turns
@@ -82,9 +81,54 @@ const flowControl = (function() {
 
         player1.hasTheTurn = (!player1.hasTheTurn)
         player2.hasTheTurn = (!player2.hasTheTurn)
+
+        checkGameStatus()
     }
 
     // * Checking for win || draw
+    const checkGameStatus = function() {
+        console.log('I checked the gameStatus');
+        
+        if (!gameboard.stateOfGameboard.some(value => value === '')) {
+            console.log('the game is a draw');
+            return;
+        }
+        function getRows(stateOfGameboard) {
+            let valuesOfGameboard= [];
+            let valuesOfColumns = [];
+            let valuesOfDiagonals = [];
+
+            // * Get Rows
+            for (let i = 0; i < stateOfGameboard.length; i += 3) {
+                valuesOfGameboard.push(stateOfGameboard.slice(i, i + 3));
+            } // * Get Columns
+            for (let i = 0; i < 3; i++) {
+                valuesOfGameboard.push([stateOfGameboard[i], stateOfGameboard[i + 3], stateOfGameboard[i + 6]]);
+            } // * Get diagonals
+            for (let i = 0; i < 3; i += 2) {
+                if (i === 0) valuesOfGameboard.push([stateOfGameboard[i], stateOfGameboard[i + 4], stateOfGameboard[i + 8]]);
+                if (i === 2) valuesOfGameboard.push([stateOfGameboard[i], stateOfGameboard[i + 2], stateOfGameboard[i + 4]]);
+            }
+
+            return [...valuesOfGameboard]
+        }
+        const rowsOfGameboard = getRows(gameboard.stateOfGameboard)
+
+        function checkIfSomeoneWon(rowsOfGameboard) {
+            const winnigArray = rowsOfGameboard.map(valueArr => {
+                return valueArr.every(e => {
+                    return e === 'X' || e === 'O';
+                });
+            });
+            
+            const winnerIndex = winnigArray.findIndex(e => e ===true);
+            if (winnerIndex === -1) return;
+
+            const winnerSymbol = rowsOfGameboard[winnerIndex][0];
+            console.log(`The winner is ${winnerSymbol}`); 
+        }
+        checkIfSomeoneWon(rowsOfGameboard)
+    }
 
     // * Announcing winner & restarting option
 
@@ -152,4 +196,4 @@ const trefortosFeladatGyak = (function() {
         trefortFeladat,
     }
 })()
-console.log(trefortosFeladatGyak.trefortFeladat(40))
+// console.log(trefortosFeladatGyak.trefortFeladat(40))
